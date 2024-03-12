f523465cfe3a2ec67af7c2969c363fe25469bb3f
JosueSerrano <jserranor1800@alumno.ipn.mx>
2024-03-11 19:40:32 -0600

    Mejoras pila

--- Parcial1/Tarea2.c
+++ Parcial1/Tarea2.c
@@ -13,10 +13,71 @@ typedef struct pila{
     int sizePila;
 }Pila;
 
+void eliminarPila(Pila* p);
+int isEmpty(Pila *p);
+void push(Pila* p, Nodo n);
+int pop(Pila*p);
+int conocerTope(Pila *p);
+void imprimirPila(Pila*p);
+int sizePila(Pila* p);
+
+// Funcion principal
+int main(){
+
+    // Inicializar la pila
+    Pila pila;
+    pila.tope = NULL;
+    pila.sizePila = 0;
+    Nodo n;
+    
+    printf("Introduce un valor: ");
+    scanf("%d",&n.val);
+
+    push(&pila,n);
+
+    printf("Introduce un valor: ");
+    scanf("%d",&n.val);
+
+    push(&pila,n);
+
+    printf("Pila con valores (push): \n");
+    imprimirPila(&pila);
+
+    printf("\nConocer el valor de tope: %d",conocerTope(&pila));
+
+    printf("\nTamanio de la pila actual: %d",sizePila(&pila));
+
+    // Quitar el primer valor dentro de la pila
+    printf("\nPila quitando el valor de tope (pop): \n");
+    pop(&pila);
+
+    imprimirPila(&pila);
+
+    // Conocer el tamaño de la pila actual
+    printf("\nTamanio de la pila actual: %d",sizePila(&pila));
+
+    // Saber si la pila esta vacia
+    if(isEmpty(&pila)){
+        printf("\nLa pila esta vacia");
+    }
+    else{
+        printf("\nLa pila no esta vacia");
+    }
+}
+
+
+int isEmpty(Pila *p){
+    int resp = 1;
+    if(p->tope != NULL){
+        resp = 0;
+    }
+    return resp;
+}
+
 // Funcion para ingresar valores en la pila
-void push(Pila* p, int val){
+void push(Pila* p, Nodo n){
     Nodo *nuevoNodo = (Nodo*)malloc(sizeof(Nodo));
-    nuevoNodo->val = val;
+    nuevoNodo->val = n.val;
     nuevoNodo->sig = p->tope;
     p->tope = nuevoNodo;
     p->sizePila++;
@@ -25,6 +86,10 @@ void push(Pila* p, int val){
 // Funcion para eliminar valores en la pila
 int pop(Pila*p){
     Nodo* temp = (Nodo*)malloc(sizeof(Nodo));
+    if(p->tope == NULL){
+        printf("La pila esta vacia");
+        return -1;
+    }
     int val = p->tope->val;
     temp = p->tope;
     p->tope = p->tope->sig;
@@ -45,10 +110,16 @@ int conocerTope(Pila *p){
 // Funcion para imprimir los valores dentro de la pila
 void imprimirPila(Pila*p){
     Nodo *temp = p->tope;
-    while(temp != NULL){
-        printf("%d ",temp->val);
-        temp = temp->sig;
+    if(isEmpty(p)){
+        printf("La pila esta vacia");
     }
+    else{
+        while(temp != NULL){
+            printf("%d ",temp->val);
+            temp = temp->sig;
+        }
+    }
+    
 }
 
 // Funcion para conocer el tamaño de la pila
@@ -60,31 +131,13 @@ int sizePila(Pila* p){
     return p->sizePila;
 }
 
-// Funcion principal
-int main(){
-    // Inicializar la pila
-    Pila pila;
-    pila.tope = NULL;
-    pila.sizePila = 0;
 
-    // Ingresar valores dentro de la pila
-    push(&pila,15);
-    push(&pila,14);
-    push(&pila,12);
-    push(&pila,7);
-    printf("Pila con valores (push): \n");
-    imprimirPila(&pila);
-
-    printf("\nConocer el valor de tope: %d",conocerTope(&pila));
-
-    printf("\nTamanio de la pila actual: %d",sizePila(&pila));
-
-    // Quitar el primer valor dentro de la pila
-    printf("\nPila quitando el valor de tope (pop): \n");
-    pop(&pila);
-
-    imprimirPila(&pila);
-
-    // Conocer el tamaño de la pila actual
-    printf("\nTamanio de la pila actual: %d",sizePila(&pila));
+void eliminarPila(Pila* p){
+    Nodo* temp;
+    while(p->tope != NULL){
+        temp = p->tope;
+        p->tope = p->tope->sig;
+        free(temp);
+        p->sizePila--;
+    }
 }
